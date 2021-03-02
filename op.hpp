95fa4349ad011c1cc6736d9d6fb59e9f7ab39eac
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-03-02 11:23:04 -0800

    Update op.hpp

--- op.hpp
+++ op.hpp
@@ -7,7 +7,12 @@ class Op : public Base {
     public:
         Op(double value) : Base(), val(value) { }
         virtual double evaluate() { return val; }
-	 void accept(Visitor* visitor, int index) {          if (index == 0) {                  visitor->visit_add_begin(this); }          else if (index == 1) {                  visitor->visit_add_middle(this); }          else if (index == 2) { visitor->visit_add_end(this); }  }
+	 void accept(Visitor* visitor, int index) {          
+		 if (index == 0) {                  
+			 visitor->visit_op(this); }          
+		 else if (index == 1) {                  
+			 visitor->visit_op(this); }          
+	 }
         virtual std::string stringify() { return std::to_string(val); }
     private:
 	double val;
