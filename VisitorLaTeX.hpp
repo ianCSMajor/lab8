59c0529f46d399621d4bdae4b23452dc1c20d2ca
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-03-01 23:04:13 -0800

    Update VisitorLaTeX.hpp

--- VisitorLaTeX.hpp
+++ VisitorLaTeX.hpp
@@ -6,4 +6,4 @@ std::string PrintLaTex(Div* ptr) {
 	return ("${\frac{" + val1 -> stringify() + "}{" + val2 -> stringify() + "}}$");         
 } 
 std::string PrintLaTex(Mult* ptr) {             
-	return ("${({" + val1 -> stringify() + "}^{" + val2 -> stringify() + "})}$");         }  };
+	return ("${({" + val1 -> stringify() + "}\cdot{" + val2 -> stringify() + "})}$");         }  };
