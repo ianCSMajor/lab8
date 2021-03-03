b5421ada92b2216e93fff5cc78bb71fecebd2d64
Kiet Lam <33382487+LKietam@users.noreply.github.com>
2021-03-03 00:16:19 -0800

    Update base.hpp

--- base.hpp
+++ base.hpp
@@ -2,7 +2,7 @@
 #define __BASE_HPP__
 
 #include <string>
-#include "visitor.hpp"
+
 class Base {
     public:
         virtual ~Base() = default;
@@ -10,11 +10,7 @@ class Base {
         /* Pure Virtual Functions */
         virtual double evaluate() = 0;
         virtual std::string stringify() = 0;
-        virtual void accept(Visitor* visitor, int index) = 0;
         virtual int number_of_children() = 0;
-
-	      //virtual void visit_all(Visitor* v) = 0;
-
         virtual Base* get_child(int i) = 0;
 };
 
