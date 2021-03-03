bbc2d85750c6e46dd1961572e979119a3233d921
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-03-02 17:09:17 -0800

    Update VisitorLaTeX.hpp

--- VisitorLaTeX.hpp
+++ VisitorLaTeX.hpp
@@ -1,15 +1,25 @@
 class VisitorLaTeX: public Visitor{  
+	private:
+	Base* val1;
+	Base* val2;
+	public:
+	VisitorLaTeX(Base* initialVal, Base* subVal): Base(), val1(initialVal), val2(subVal) {}
+	
 	std::string PrintLaTex(Pow* ptr) {             
-		return ("${({" + val1 -> stringify() + "}^{" + val2 -> stringify() + "})}$");         
-} 
-std::string PrintLaTex(Div* ptr) {             
-	return ("${\frac{" + val1 -> stringify() + "}{" + val2 -> stringify() + "}}$");         
-} 
-std::string PrintLaTex(Sub* ptr) {             
-	return ("${({" + val1 -> stringify() + "}-{" + val2 -> stringify() + "})}$");         
-} 
-std::string PrintLaTex(Add* ptr) {             
-	return ("${({" + val1 -> stringify() + "}+{" + val2 -> stringify() + "})}$");         
-} 
-std::string PrintLaTex(Mult* ptr) {             
-	return ("${({" + val1 -> stringify() + "}\cdot{" + val2 -> stringify() + "})}$");         }  };
+			return ("${({" + val1 -> stringify() + "}^{" + val2 -> stringify() + "})}$");         
+	} 
+	
+	std::string PrintLaTex(Div* ptr) {             
+		return ("${\frac{" + val1 -> stringify() + "}{" + val2 -> stringify() + "}}$");         
+	} 
+	
+	std::string PrintLaTex(Sub* ptr) {             
+		return ("${({" + val1 -> stringify() + "}-{" + val2 -> stringify() + "})}$");         
+	} 
+	
+	std::string PrintLaTex(Add* ptr) {             
+		return ("${({" + val1 -> stringify() + "}+{" + val2 -> stringify() + "})}$");         
+	} 
+	
+	std::string PrintLaTex(Mult* ptr) {             
+		return ("${({" + val1 -> stringify() + "}\cdot{" + val2 -> stringify() + "})}$");         }  };
