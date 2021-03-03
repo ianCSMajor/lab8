79924075ced07b170d40120efc68e90e33ee1da6
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-03-02 19:39:41 -0800

    Update base.hpp

--- base.hpp
+++ base.hpp
@@ -10,6 +10,7 @@ class Base {
         /* Pure Virtual Functions */
         virtual double evaluate() = 0;
         virtual std::string stringify() = 0;
+        virtual void accept(Visitor* visitor, int index) = 0;
         virtual int number_of_children() = 0;
         virtual Base* get_child(int i) = 0;
 };
