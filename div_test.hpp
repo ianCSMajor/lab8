b24731d4455dfbc2deb2e473342913a0cceee3b5
Kiet Lam <33382487+LKietam@users.noreply.github.com>
2021-03-03 15:18:14 -0800

    Update div_test.hpp

--- div_test.hpp
+++ div_test.hpp
@@ -10,6 +10,9 @@ TEST(DivTest, DivEvaluatePos) {
     Base* val2 = new Op(4);
     Base* test = new Div(val1,val2);
     EXPECT_NEAR(test -> evaluate(), 1.75, 0.001);
+    delete val1;
+    delete val2;
+    delete test;
 }
 
 TEST(DivTest, DivPrintPos) {
@@ -17,24 +20,36 @@ TEST(DivTest, DivPrintPos) {
     Base* val2 = new Op(4);
     Base* test = new Div(val1,val2);
     EXPECT_EQ(test -> stringify(), "7.000000 / 4.000000");
+    delete val1;
+    delete val2;
+    delete test;
 }
 TEST(DivTest, DivGetChildOne) {
     Base* val1 = new Op(7);
     Base* val2 = new Op(4);
     Base* test = new Div(val1,val2);
     EXPECT_EQ(test -> get_child(1)->evaluate(), 7);
+    delete val1;
+    delete val2;
+    delete test;
 }
 TEST(DivTest, DivGetChildTwo) {
     Base* val1 = new Op(7);
     Base* val2 = new Op(4);
     Base* test = new Div(val1,val2);
     EXPECT_EQ(test -> get_child(2)->evaluate(), 4);
+    delete val1;
+    delete val2;
+    delete test;
 }
 TEST(DivTest, DivGetNumberChildren) {
     Base* val1 = new Op(7);
     Base* val2 = new Op(4);
     Base* test = new Div(val1,val2);
     EXPECT_EQ(test -> number_of_children(), 2);
+    delete val1;
+    delete val2;
+    delete test;
 }
 
 #endif
