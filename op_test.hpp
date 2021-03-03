ba01fdbea560334ced939ff20ff7238ff7277976
Kiet Lam <33382487+LKietam@users.noreply.github.com>
2021-03-03 15:12:44 -0800

    Update op_test.hpp

--- op_test.hpp
+++ op_test.hpp
@@ -8,19 +8,23 @@
 TEST(OpTest, OpEvaluateNonZero) {
     Base* test = new Op(8);
     EXPECT_EQ(test->evaluate(), 8);
+    delete test;
 }
 
 TEST(OpTest, OpPrintNonZero) {
     Base* test = new Op(8);
     EXPECT_EQ(test->stringify(), "8.000000");
+    delete test;
 }
 TEST(OpTest, OpGetChildZero) {
     Base* test = new Op(7);
     EXPECT_EQ(test -> get_child(2), nullptr);
+    delete test;
 }
 TEST(OpTest, OpGetNumberChildren) {
     Base* test = new Op(7);
     EXPECT_EQ(test -> number_of_children(), 0);
+    delete test;
 }
 
 #endif
